be1ad5dea33e980978f8a9ff03d7d9c55ff915c6
Jaffar Hussein <jaffhussein@icloud.com>
2022-06-18 11:03:28 +0200

    ft:Created the user and neighbourhood models

--- logic/models.py
+++ logic/models.py
@@ -1,3 +1,16 @@
+from email.policy import default
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+
+
+class Neighbourhood(models.Model):
+    name = models.CharField(max_length=50, null=False, unique=True)
+    location = models.CharField(max_length=50, null=False)
+    # occupants = models.IntegerField(default=0)
+    admin = models.ForeignKey(User, null=False, related_name='neighbourhoods',on_delete=models.CASCADE)
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, related_name="users",on_delete=models.CASCADE)
+    neighbourhood = models.ForeignKey(Neighbourhood, related_name="occupants",on_delete=models.CASCADE)
--- logic/views.py
+++ logic/views.py
@@ -1,3 +1,6 @@
+from django.http import HttpResponse, HttpResponseNotAllowed
 from django.shortcuts import render
 
-# Create your views here.
+
+def home(request):
+    return HttpResponse()
